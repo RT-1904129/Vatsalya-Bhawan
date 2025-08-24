5c94394e8f8f9d19f535ba6fd4fe70ceafb2c15a
Rishabh Tripathi <rishabh29072001@gmail.com>
2025-08-24 14:39:29 +0530

    spike changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import VatsalayaBhawan from './VatsalayaBhawan';
 
 function App() {
   return (
       <div>
-        <MainPage />
+        <VatsalayaBhawan />
       </div>
   );
 }
